673a87f0d8777e38e9feeb09d2947e0ee3414e61
almccutc <63007467+almccutc@users.noreply.github.com>
2020-05-26 18:34:56 -0500

    Add files via upload

---
+++ PIVanalysis_run.m
@@ -0,0 +1,21 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% script for testing PIVanalysis and methods
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear, clc
+delete all
+
+% FFD_test.computeUStar;
+
+PIVanalysis_test=PIVanalysis();PIVanalysis_test.Main();
+
+% % this will initialize PIVanalysis_test with all properties and methods defined in
+% % PIVanalysis with all default definitions. it will also run the object 
+% % initialization method.
+
+
+      
+      
+      
+      
+      
+      
